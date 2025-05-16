d9032a8a7eb31b6596c96083e0ae3aa2a266881e
Büşra Atasoy <bsraklcsln@gmail.com>
2025-05-16 14:33:33 +0300

    Create practice_with_robot_alone_area.py

---
+++ Day-06/codes/practice_with_robot_alone_area.py
@@ -0,0 +1,17 @@
+# Lines of code written using the def function:
+def turn_around():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+#Draw square
+turn_left()   
+move()
+turn_around()
+move()
+turn_around()
+move()
+turn_around()
+move()
+turn_left() 
