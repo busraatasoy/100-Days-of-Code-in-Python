44f41350da0e82140b7a3834f67fafce2574867d
Büşra Atasoy <bsraklcsln@gmail.com>
2025-05-16 14:59:01 +0300

    Create loops_while.py

---
+++ Day-06/codes/loops_while.py
@@ -0,0 +1,25 @@
+# Loops: While :
+# What is a Loop?
+'''
+- **Loops** are used to execute a block of code repeatedly.
+    
+# **while Loop:**  
+- Runs as long as a specified condition is **True.** '''
+    
+# Create while Loop:
+while something_is_true:
+    #Do something repeatedly
+
+#Example:
+x = 0
+while x < 5:  # Runs until x is 5
+    print(x)
+    x += 1
+  
+#Special Keywords in Loops:
+'''
+- `break`: Stops the loop immediately.
+- `continue`: Skips the current iteration and moves to the next one.
+- `pass`: Does nothing, used as a placeholder.
+
+In short, **loops help automate repetitive tasks**, making your code more efficient. '''
