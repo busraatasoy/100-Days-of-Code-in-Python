96547efb8ade31547765c312414fd43448462c49
Büşra Atasoy <bsraklcsln@gmail.com>
2025-05-16 10:15:26 +0300

    Create the_gauss_challenge.py

---
+++ Day-05/codes/the_gauss_challenge.py
@@ -0,0 +1,6 @@
+# Scenario: Work out the total numbers between 1 and 100, inclusive of both 1 and 100.
+
+total = 0
+for number in range(1, 101)
+      total += number 
+print(total)                          #Result: 5050
