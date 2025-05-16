4d3cb8092dfc15586054204b8af51f40212e88a7
Büşra Atasoy <bsraklcsln@gmail.com>
2025-05-16 15:19:36 +0300

    Create Infinite_loop.py

---
+++ Day-06/codes/Infinite_loop.py
@@ -0,0 +1,10 @@
+#Infinite Loop:
+'''    
+- An infinite loop occurs when the condition of a loop (such as `while` or `for`) is always true, causing the loop to repeat forever without stopping.
+- It happens when there’s no way for the loop's condition to eventually become false. '''
+    
+#Example:
+while 5 > 3:
+print("This loop will run forever!")
+
+#This loop will keep printing "This loop will run forever!" because the condition 5 > 3 is always true.
