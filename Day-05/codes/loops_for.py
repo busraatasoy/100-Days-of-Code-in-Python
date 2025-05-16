5318f5c6dd8a9d5c4f99e82b017a107936446cfb
Büşra Atasoy <bsraklcsln@gmail.com>
2025-05-16 10:08:31 +0300

    Create loops_for.py

---
+++ Day-05/codes/loops_for.py
@@ -0,0 +1,26 @@
+#What is a Loop?
+#Loops are used to execute a block of code repeatedly.
+
+# for Loop:
+#Used to iterate over a sequence (such as a list, string, or range).'''
+
+# Creating a for Loop:
+for item in list_of_items:
+    #Do something to each item
+
+#Example of Fruits:
+#List of fruits
+fruits = ["Apple", "Peach" , "Pear"]
+
+#Each item in the list is printed with for
+for fruit in fruits:
+    print(fruit)
+    print(fruit + "pie" )
+
+# Special Keywords in Loops:
+
+# break: Stops the loop immediately.
+# continue: Skips the current iteration and moves to the next one.
+# pass: Does nothing, used as a placeholder.
+
+#In short, **loops help automate repetitive tasks**, making your code more efficient. 
